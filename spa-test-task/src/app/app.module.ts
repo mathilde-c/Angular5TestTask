bde945233d201ab568a61e1664fe0dea770266e4
Mathilde <ceccaroli.m@gmail.com>
2018-02-11 11:02:02 +1000

    [#1] add necessary package for development of component

--- spa-test-task/src/app/app.module.ts
+++ spa-test-task/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { DatesFilterComponent } from './components/dates-filter/dates-filter.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatesFilterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, 
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
