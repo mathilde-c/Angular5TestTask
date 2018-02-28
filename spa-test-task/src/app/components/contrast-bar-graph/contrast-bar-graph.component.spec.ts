1897d0310d0572293249ef1589485666483ec1ce
Mathilde <ceccaroli.m@gmail.com>
2018-02-28 16:30:42 +1000

    [#17] Basic for component with no dependencies

--- spa-test-task/src/app/components/contrast-bar-graph/contrast-bar-graph.component.spec.ts
+++ spa-test-task/src/app/components/contrast-bar-graph/contrast-bar-graph.component.spec.ts
@@ -3,23 +3,57 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { ContrastBarGraphComponent } from "./contrast-bar-graph.component";
 
 describe("ContrastBarGraphComponent", () => {
-  let component: ContrastBarGraphComponent;
-  let fixture: ComponentFixture<ContrastBarGraphComponent>;
+    let component: ContrastBarGraphComponent;
+    let fixture: ComponentFixture<ContrastBarGraphComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ContrastBarGraphComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ContrastBarGraphComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ContrastBarGraphComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ContrastBarGraphComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
+
+    it("should compute correct ration for pass when total is not zero", () => {
+        component.pass = 7;
+        component.fail = 3;
+        expect(component.getPassRate()).toEqual(70);
+    });
+
+    it("should compute correct ration for pass when total is not zero", () => {
+        component.pass = 7;
+        component.fail = 7;
+        expect(component.getFailRate()).toEqual(50);
+    });
+
+    it("should return 0 for pass when total fail + pass is zero", () => {
+        component.pass = 0;
+        component.fail = 0;
+        expect(component.getPassRate()).toEqual(0);
+    });
+
+    it("should return 0 for fail when total fail + pass is zero", () => {
+        component.pass = 0;
+        component.fail = 0;
+        expect(component.getFailRate()).toEqual(0);
+    });
+
+    it("should return 0 for pass when total fail or pass is undefined", () => {
+        component.fail = 20;
+        expect(component.getPassRate()).toEqual(0);
+    });
+
+    it("should return 0 for fail when total fail or pass is undefined", () => {
+        component.pass = 0.5;
+        expect(component.getFailRate()).toEqual(0);
+    });
 });
--- spa-test-task/src/app/components/contrast-bar-graph/contrast-bar-graph.component.ts
+++ spa-test-task/src/app/components/contrast-bar-graph/contrast-bar-graph.component.ts
@@ -1,27 +1,25 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "app-contrast-bar-graph",
     templateUrl: "./contrast-bar-graph.component.html",
     styleUrls: ["./contrast-bar-graph.component.css"]
 })
-export class ContrastBarGraphComponent implements OnInit {
+export class ContrastBarGraphComponent {
     @Input() public pass: number;
     @Input() public fail: number;
 
-    private total: number;
-
-    public ngOnInit(): void {
-        this.total = this.pass + this.fail;
-    }
+    private get total(): number { return this.pass + this.fail; }
 
     public getPassRate(): number {
-        return this.total !== 0
+        return (this.total
+            && this.total !== 0)
             ? (this.pass / this.total) * 100.0
             : 0;
     }
     public getFailRate(): number {
-        return this.total !== 0
+        return (this.total
+            && this.total !== 0)
             ? (this.fail / this.total) * 100.0
             : 0;
     }
