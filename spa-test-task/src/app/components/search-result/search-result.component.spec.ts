79406a92719f245dcbb114a7032c19f8337b74af
Mathilde <ceccaroli.m@gmail.com>
2018-02-28 20:28:29 +1000

    [#17] introduce mock component & stub service in test
    - use ng2-mock-component library

--- spa-test-task/src/app/components/search-result/search-result.component.spec.ts
+++ spa-test-task/src/app/components/search-result/search-result.component.spec.ts
@@ -1,25 +1,51 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+import { MatTooltipModule, MatTableModule, MatPaginatorModule } from "@angular/material";
+import { MockComponent } from "ng2-mock-component/index";
 
 import { SearchResultComponent } from "./search-result.component";
+import { FilterService } from "../../services/filter.service";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { IItemCompletedAuditSearchResult } from "../../models/item-completed-audit-search-result";
 
 describe("SearchResultComponent", () => {
-  let component: SearchResultComponent;
-  let fixture: ComponentFixture<SearchResultComponent>;
+    let component: SearchResultComponent;
+    let fixture: ComponentFixture<SearchResultComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SearchResultComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        let filterServiceStub: Partial<FilterService>;
+        filterServiceStub = {
+            upToDateSearchResults: new BehaviorSubject<Array<IItemCompletedAuditSearchResult>>([]),
+            upToDateSearchResultsTitle: new BehaviorSubject<string>("")
+        };
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SearchResultComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        TestBed.configureTestingModule({
+            declarations: [
+                SearchResultComponent,
+                MockComponent({ selector: "app-contrast-bar-graph", inputs: ["pass", "fail"] }),
+            ],
+            imports: [
+                BrowserModule,
+                BrowserAnimationsModule,
+                FormsModule,
+                MatTooltipModule,
+                MatTableModule,
+                MatPaginatorModule,
+            ],
+            providers: [{ provide: FilterService, useValue: filterServiceStub }]
+        })
+            .compileComponents();
+    }));
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SearchResultComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
