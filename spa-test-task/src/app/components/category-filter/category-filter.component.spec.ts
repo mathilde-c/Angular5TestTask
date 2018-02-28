7649d41d95af1495eb495f77fef85f2535d5e21f
Mathilde <ceccaroli.m@gmail.com>
2018-02-28 16:45:09 +1000

    [#17] test clear-component

--- spa-test-task/src/app/components/category-filter/category-filter.component.spec.ts
+++ spa-test-task/src/app/components/category-filter/category-filter.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { CategoryFilterComponent } from "./category-filter.component";
 
 describe("CategoryFilterComponent", () => {
-  let component: CategoryFilterComponent;
-  let fixture: ComponentFixture<CategoryFilterComponent>;
+    let component: CategoryFilterComponent;
+    let fixture: ComponentFixture<CategoryFilterComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CategoryFilterComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [CategoryFilterComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CategoryFilterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CategoryFilterComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
--- spa-test-task/src/app/components/clear-filter/clear-filter.component.spec.ts
+++ spa-test-task/src/app/components/clear-filter/clear-filter.component.spec.ts
@@ -1,25 +1,41 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import "rxjs/add/operator/takeWhile";
 
 import { ClearFilterComponent } from "./clear-filter.component";
 
 describe("ClearFilterComponent", () => {
-  let component: ClearFilterComponent;
-  let fixture: ComponentFixture<ClearFilterComponent>;
+    let component: ClearFilterComponent;
+    let fixture: ComponentFixture<ClearFilterComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ClearFilterComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ClearFilterComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ClearFilterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ClearFilterComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
+
+    it("should emit \"clearFiltersTriggered\" event when clicked ", (done) => {
+        let unsuscribe: boolean = true;
+        component.clearFiltersTriggered
+            .takeWhile(() => unsuscribe)
+            .subscribe(event => {
+                expect(event).toBeTruthy();
+                unsuscribe = false;
+                done();
+            });
+
+        const button = document.querySelector("button") as HTMLElement;
+
+        button.click();
+    });
 });
